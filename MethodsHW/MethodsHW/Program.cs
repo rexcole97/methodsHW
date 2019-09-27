abb1b0edf84fcf9c6060b7fd28bf7caf968a4415
rexcole97 <54370507+rexcole97@users.noreply.github.com>
2019-09-27 14:33:02 -0500

    Add files via upload

---
+++ MethodsHW/MethodsHW/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace MethodsHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prog = new Program();
+            prog.myhomework();
+            prog.myhomework2(150, 5);
+
+        }
+        public void myhomework()
+        {
+            console.writeline("Homework is lovely");
+        }
+        public void myhomework2 (float param1f, float param2f)
+        {
+            var average = param1f / param2f;
+            console.writeline(average); 
+        }
+    }
+}
